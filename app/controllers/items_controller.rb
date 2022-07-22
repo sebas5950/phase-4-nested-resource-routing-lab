2116401719768631f94aad0e8c0dfb77804de4fa
Alex Guanipatin <sebas5950@gmail.com>
2022-07-22 13:07:36 -0400

    commit tree 07d300bde03a0f6d95a71630190e432ed2ce2cfa

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+      render json: items
+    else
+      items = Item.all
+      render json: items, include: :user
+      byebug
+    end
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(strong_params)
+    render json: item, status: :created
+  end
+
+  def show
+    user = User.find(params[:user_id])
+    item = user.items.find(params[:id])
+    render json: item
+  end
+
+  private
+
+  def strong_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found
+    render json: { error: "Item not found" }, status: :not_found
   end
 
 end
